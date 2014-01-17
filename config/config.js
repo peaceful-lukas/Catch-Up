154cc62c96c722f983f33fae66b3d5d0e485ed17
luaks <peaceful.lukas@gmail.com>
2014-01-17 14:12:51 +0900

    config.js 수정

--- config/config.js
+++ config/config.js
@@ -1,15 +1,9 @@
 module.exports = {
   development: {
-    db: {
-      host: 'mongodb://192.168.10.145/catchup-dev' // 로컬서버.
-    },
     root: require('path').normalize(__dirname + '/..')
   },
   
   production: {
-    db: {
-      host: 'mongodb://heroku_app21208894:n67c2mbl0n8f9oucum84ngm43r@ds061248.mongolab.com:61248/heroku_app21208894'
-    },
     root: require('path').normalize(__dirname + '/..')
   }
 }
